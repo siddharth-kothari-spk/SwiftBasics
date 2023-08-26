2d77d045c0c352848461807e913056bcc9067a69
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-26 16:12:54 +0530

    Pass through subject example

---
+++ Combine/5. Publishing property changes in Combine.playground/Contents.swift
@@ -0,0 +1,14 @@
+/// courtsey: https://www.donnywals.com/publishing-property-changes-in-combine/
+/// In Combine, everything is considered a stream of values that are emitted over time. This means that sometimes a publisher can publish many values, and other times it publishes only a single value. And other times it errors and publishes no values at all. When your UI has to respond to changing data, or if you want to update your UI in response to a user's actions, you might consider the data and user input to both be streams of values.
+import Combine
+
+// There are no dedicated publishers for your own objects and properties. There are, however, publishers that allow you to publish objects of a certain type at will. These publishers are the PassthroughSubject and CurrentValueSubject publishers.
+
+// 1. Using a PassthroughSubject to publish values
+var stringSubject = PassthroughSubject<String, Never>()
+stringSubject.sink { value in
+    print("Received value: \(value)")
+}
+
+stringSubject.send("Sid")
+stringSubject.send("Kots")

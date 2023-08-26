82c8c9f2c9a1c62c0da3536d0e15ceb849ac44d8
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-26 16:49:50 +0530

    assign(to:on:) with MVVM

--- Combine/6. Updating UI with assign(to-on-) in Combine.playground/Contents.swift
+++ Combine/6. Updating UI with assign(to-on-) in Combine.playground/Contents.swift
@@ -31,3 +31,32 @@ func subscribeToCarCharge() {
 
 // It communicates that we want to map the Double that is provided by $kwhInBattery into a String, and that we want to assign that string to the text property on label. The assign(to:on:) method returns an AnyCancellable, just like sink. So we need to retain it to make sure it doesn't get deallocated.
 
+
+// Using assign(to:on:) becomes very interesting if you use an architecture where your model prepares data for your view in a way where no further processing is required, like MVVM:
+struct CarViewModel {
+  private let car = Car()
+
+  let chargeRemainingText: AnyPublisher<String?, Never>
+
+  init() {
+    chargeRemainingText = car.$kwhInBattery.map {
+      "Car's charge is \($0)"
+    }.eraseToAnyPublisher()
+  }
+}
+
+class CarViewController2: UIViewController {
+  let viewModel = CarViewModel()
+  let label = UILabel()
+
+  var cancellables = Set<AnyCancellable>()
+
+    func subscribeToCarCharge() {
+    viewModel.chargeRemainingText
+        .assign(to: \.text, on: label)
+        .store(in: &cancellables)
+    }
+
+  // More VC code...
+}
+

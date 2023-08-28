cf207c0678e5d822f84b95dbb04b6251d1021bdb
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-28 18:44:46 +0530

    quality of service

--- Concurrency/2. Dispatch Queues.playground/Contents.swift
+++ Concurrency/2. Dispatch Queues.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
-
+/*
 DispatchQueue.main.async {
     print(Thread.isMainThread ? "Main thread" : "Not main thread")
 }
@@ -14,4 +14,17 @@ DispatchQueue.global().async {
 
 DispatchQueue.global(qos: .userInteractive).async {
     print(Thread.isMainThread ? "Main thread" : "global concurrent queue - userInteractive")
+}
+*/
+//
+DispatchQueue.global(qos: .background).async {
+    for i in 11...21 {
+        print(i)
+    }
+}
+
+DispatchQueue.global(qos: .userInteractive).async {
+    for i in 0...10 {
+        print(i)
+    }
 }

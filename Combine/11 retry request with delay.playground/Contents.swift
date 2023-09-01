9ca374b4cd6c699c2d3245f2c4e61f27f8ff0bfe
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-01 16:44:37 +0530

    handling valid network failures

--- Combine/11 retry request with delay.playground/Contents.swift
+++ Combine/11 retry request with delay.playground/Contents.swift
@@ -24,3 +24,30 @@ dataTaskPublisher
 // Note that a 404, 501 or any other error status code does not count as a failed request in Combine. The request made it to the server and the server responded. A failed request typically means that the request wasn't executed because the device making the request is offline, the server failed to respond in a timely manner, or any other reason where we never received a response from the server.
 
 
+// how should an HTTP status code of 429 (Too Many Requests / Rate Limit) or 503 (Server Busy) be handled? These will be seen as successful outcomes by Combine so we'll need to inspect the server's response, raise an error and retry the request with a couple of seconds delay since we don't want to make the server even busier than it already is (or continue hitting our rate limit).
+
+enum DataTaskError: Error {
+  case invalidResponse, rateLimitted, serverBusy
+}
+
+let dataTaskPublisher = URLSession.shared.dataTaskPublisher(for: url)
+  .tryMap({ response -> (data: Data, response: URLResponse) in
+    // just so we can debug later in the post
+    print("Received a response, checking status code")
+
+    guard let httpResponse = response.response as? HTTPURLResponse else {
+      throw DataTaskError.invalidResponse
+    }
+
+    if httpResponse.statusCode == 429 {
+      throw DataTaskError.rateLimitted
+    }
+
+    if httpResponse.statusCode == 503 {
+      throw DataTaskError.serverBusy
+    }
+
+    return response
+  })
+
+

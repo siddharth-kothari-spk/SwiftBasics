8f8c7f7ca9d52b8a07e91e0e4f1697d26964a3e1
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-05 05:19:08 +0530

    updating Recursive loader to have 2 publishers

--- Combine/13 Recursively execute a paginated network call.playground/Contents.swift
+++ Combine/13 Recursively execute a paginated network call.playground/Contents.swift
@@ -32,7 +32,7 @@ struct Item {}
 // My loader should keep making more requests until it receives a Response that has its hasMorePages set to false. At that point, the chain is considered complete and the publisher created in loadPages() should emit all fetched values and complete.
 
 // model class
-
+/*
 class RecursiveLoader {
     var requestsMade = 0
     var cancellables = Set<AnyCancellable>()
@@ -53,4 +53,56 @@ class RecursiveLoader {
             }))
         }.eraseToAnyPublisher()
     }
+    
+    func finishedPublisher() -> AnyPublisher<Response, Never> {
+        Future {promise in
+            
+        }.eraseToAnyPublisher()
+    }
+    
+    func initialLoadSequence() {
+        
+    }
+}*/
+
+
+// Attempt one: using reduce operator
+// When you apply reduce to a publisher in Combine you can accumulate all emitted values into one new value that's emitted when the upstream publisher completes.
+// To load all pages I would create an instance of RecursiveLoader, subscribe to a publisher that I'd define as a property on RecursiveLoader and tell it to begin loading
+
+// updating RecursiveLoader
+class RecursiveLoader {
+  var requestsMade = 0
+
+  private let loadedPagePublisher = PassthroughSubject<Response, Never>()
+  let finishedPublisher: AnyPublisher<[Item], Never>
+
+  var cancellables = Set<AnyCancellable>()
+
+  init() {
+    self.finishedPublisher = loadedPagePublisher
+      .reduce([Item](), { allItems, response in
+        return response.items + allItems
+      })
+      .eraseToAnyPublisher()
+      
+//      loadedPagePublisher.reduce([Item]()) { allItems, response in
+//          return response.items + allItems
+//      }.eraseToAnyPublisher()
+  }
+
+  func initiateLoadSequence() {
+    // do something
+  }
 }
+
+
+let loader = RecursiveLoader()
+loader.finishedPublisher.sink { items in
+    print("items : \(items.count)")
+}.store(in: &cancellables)
+loader.initialLoadSequence()
+
+// private loadedPagePublisher is where I decided I would publish pages as they came in from the network.
+// The finishedPublisher takes the loadedPagePublisher and applies the reduce operator. That way, once I complete the loadedPagePublisher, the finsihedPublisher will emit an array of [Item]
+

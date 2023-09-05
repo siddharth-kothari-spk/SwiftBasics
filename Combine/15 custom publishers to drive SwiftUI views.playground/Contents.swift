a4f69b2093e55890e9e8c935d0a7ef7e14d1a10c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-05 16:00:11 +0530

    data received through network

--- Combine/15 custom publishers to drive SwiftUI views.playground/Contents.swift
+++ Combine/15 custom publishers to drive SwiftUI views.playground/Contents.swift
@@ -20,3 +20,33 @@ struct NamesList: View {
 
 // Whenever the DataSource object's names array changes, NamesList will be automatically redrawn.
 
+// Now imagine that our list of names is retrieved through the network somehow and we want to load the list of names in the onAppear for NamesList1
+
+class DataSource1: ObservableObject {
+    @Published var names = [String]()
+    
+    let networkingObject = NetworkingObject()
+    let cancellables = Set<AnyCancellable>()
+    
+    func loadNames() {
+        networkingObject.loadNames()
+          .receive(on: DispatchQueue.main)
+          .sink(receiveValue: { [weak self] names in
+            self?.names = names
+          })
+          .store(in: &cancellables)
+      }
+}
+
+struct NamesList: View {
+  @ObservedObject var dataSource: DataSource1
+
+  var body: some View {
+    List(dataSource.names, id: \.self) { name in
+      Text(name)
+    }.onAppear(perform: {
+      dataSource.loadNames()
+    })
+  }
+}
+

fd335b9eca30545854a8f5b8c208fc275dda8965
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-08 17:42:50 +0530

    delete from keychain

---
+++ generic keychain wrapper.playground/Sources/Delete.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+extension KeychainManager {
+    func deleteItem(
+       ofClass itemClass: ItemClass,
+       key: String, attributes:
+       ItemAttributes? = nil) throws {
+
+           var query: [String : Any] = [
+          kSecClass as String: itemClass.rawValue,
+          kSecAttrAccount as String: key as AnyObject
+       ]
+
+       if let itemAttributes = attributes {
+          for(key, value) in itemAttributes {
+             query[key as String] = value
+          }
+       }
+
+       let result = SecItemDelete(query as CFDictionary)
+       if result != errSecSuccess {
+          throw convertError(result)
+       }
+    }
+}
+
+/*
+ The delete operation is the simplest one. Like in every other method, we create our query with the item class and the key. Try to add the extra attributes, and perform the keychain delete operation. Last, we check the result and throw an error in case that the operation wasn’t successful.
+ */

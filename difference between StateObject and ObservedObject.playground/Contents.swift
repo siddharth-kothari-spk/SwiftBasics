1d79d6d4f1e51ccfa2c2905549707e176c4582c7
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-11 17:42:11 +0530

    using StateObject

--- difference between StateObject and ObservedObject.playground/Contents.swift
+++ difference between StateObject and ObservedObject.playground/Contents.swift
@@ -42,3 +42,23 @@ struct ItemList: View {
 }
 
 // If you'd tap the Increment counter button defined in Counter a couple of times, you'd see that its Count is ... label updates everytime. If you then tap the Append item to list button that's defined in ItemList, the Count is ... label in Counter resets back to 0. The reason for this is that Counter got recreated which means that we now have a fresh instance of DataSource.
+
+// To fix this, we could either create the DataSource in ItemList, keep that instance around as a property on ItemList and pass that instance to Counter, or we can use @StateObject instead of @ObservedObject:
+
+struct CounterStateObject: View {
+  @StateObject var dataSource = DataSource()
+
+  var body: some View {
+    VStack {
+      Button("Increment counter") {
+        dataSource.counter += 1
+      }
+
+      Text("Count is \(dataSource.counter)")
+    }
+  }
+}
+
+// By making DataSource a @StateObject, the instance we create is kept around and used whenever the Counter view is recreated. This is extremely useful because ItemList doesn't have to retain the DataSource on behalf of the Counter, which makes the DataSource that much cleaner.
+
+// You should use @StateObject for any ObservableObject properties that you create yourself in the object that holds on to that object. So in this case, Counter creates its own DataSource which means that if we want to keep it around, we must mark it as an @StateObject.

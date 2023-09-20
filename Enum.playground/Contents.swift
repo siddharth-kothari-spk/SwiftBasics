67c69f5c6a9ca880976dc1ab2b16144555227c03
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-09-20 11:46:07 +0530

    Enums with associated values

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -15,3 +15,21 @@ enum Day {
 
 let currentday = Day.monday
 print(currentday)
+
+// Enums with associated values
+
+/// Enums with associated values enable us to store extra data alongside each case.
+
+enum Temperature {
+  case celsius(Double)
+  case fahrenheit(Double)
+  case kelvin(Double)
+}
+
+let tempInCelsius = Temperature.celsius(27.0)
+print(tempInCelsius)
+let tempInFahrenheit = Temperature.fahrenheit(83.0)
+print(tempInFahrenheit)
+let tempInKelvin = Temperature.kelvin(373.0)
+print(tempInKelvin)
+

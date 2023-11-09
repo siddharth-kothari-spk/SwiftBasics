fee86911c76b0ae9ed4bda6f00c399351873b876
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-11-10 05:13:33 +0530

    closure definition

---
+++ Closure refresher.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/// Courtsey : https://gist.github.com/sabapathyk7/525ae7cce276488de19a8cc7514fc6e4
+/*
+    
+    Closures are the self-contained blocks that can be passed around and used the code.
+    Capture the values and store a reference to any constants and variables
+    */
+    
+    /*
+    Syntax
+    { (<#parameters#>) -> <#return type#> in
+    
+    }
+    */
+
+

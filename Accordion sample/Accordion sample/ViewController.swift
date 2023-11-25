9fe884de5ff1849ed4e1bfbd90654df08f519321
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-11-25 17:06:39 +0530

    initial commit: accordion

---
+++ Accordion sample/Accordion sample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Accordion sample
+//
+//  Created by Siddharth Kothari on 25/11/23.
+//
+
+
+// courtsey: https://stevenpcurtis.medium.com/an-accordion-view-in-swift-and-uikit-83c93f0958df
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

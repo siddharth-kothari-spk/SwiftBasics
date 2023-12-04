53b420c4306d24983fac7b7daa4600e82e76bebd
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-04 19:12:17 +0530

    Formatters: DateIntervalFormatter

--- Formatters.playground/Contents.swift
+++ Formatters.playground/Contents.swift
@@ -1,15 +1,31 @@
 // https://www.swiftwithvincent.com/
 import Foundation
 
-// DateComponentsFormatter
-//1. https://developer.apple.com/documentation/foundation/datecomponentsformatter
+// 1. DateComponentsFormatter
+// https://developer.apple.com/documentation/foundation/datecomponentsformatter
 
-let formatter = DateComponentsFormatter()
-formatter.unitsStyle = .full
-formatter.includesApproximationPhrase = true
-formatter.includesTimeRemainingPhrase = true
-formatter.allowedUnits = [.minute]
+let dateComponentsFormatter = DateComponentsFormatter()
+dateComponentsFormatter.unitsStyle = .full
+dateComponentsFormatter.includesApproximationPhrase = true
+dateComponentsFormatter.includesTimeRemainingPhrase = true
+dateComponentsFormatter.allowedUnits = [.minute]
  
 // Use the configured formatter to generate the string.
-let outputString = formatter.string(from: 300.0) // About 5 minutes remaining
+let outputStringDateComponentsFormatter = dateComponentsFormatter.string(from: 300.0) // About 5 minutes remaining
 
+
+// 2. DateIntervalFormatter
+// https://developer.apple.com/documentation/foundation/dateintervalformatter
+
+let dateIntervalFormatter = DateIntervalFormatter()
+dateIntervalFormatter.dateStyle = .short
+dateIntervalFormatter.timeStyle = .none
+
+
+// Create two dates that are exactly 1 day apart.
+let startDate = Date()
+let endDate = Date(timeInterval: 86400, since: startDate)
+
+
+// Use the configured formatter to generate the string.
+let outputStringDateIntervalFormatter = dateIntervalFormatter.string(from: startDate, to: endDate)

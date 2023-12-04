652f336ae6c45cd30a32bff2894fce32eb1788ef
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-04 19:15:38 +0530

    Formatters: MeasurementFormatter

--- Formatters.playground/Contents.swift
+++ Formatters.playground/Contents.swift
@@ -29,3 +29,23 @@ let endDate = Date(timeInterval: 86400, since: startDate)
 
 // Use the configured formatter to generate the string.
 let outputStringDateIntervalFormatter = dateIntervalFormatter.string(from: startDate, to: endDate)
+
+// 3. MeasurementFormatter
+// https://developer.apple.com/documentation/foundation/measurementformatter
+
+let distanceInKiloMeters = 1.2
+
+// Distance is 1.2 km
+print("Distance is \(distanceInKiloMeters) km")
+
+let measurement = Measurement(
+    value: distanceInKiloMeters,
+    unit: UnitLength.kilometers
+)
+
+let measurementFormatter = MeasurementFormatter()
+
+measurementFormatter.locale = Locale(identifier: "en_US")
+
+// Distance is 0.746 mi
+print("Distance is \(measurementFormatter.string(from: measurement))")

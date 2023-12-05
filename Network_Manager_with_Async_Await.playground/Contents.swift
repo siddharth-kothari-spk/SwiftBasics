90d43d960710eafd69927ef067c3e92172145664
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-05 07:24:40 +0530

    Network Resquest and Response skeleton

---
+++ Network_Manager_with_Async_Await.playground/Contents.swift
@@ -0,0 +1,25 @@
+// courtsey: https://medium.com/@B4k3R/creating-a-basic-network-manager-with-async-await-swift-networking-made-easy-40f4e3f317b6
+
+import Foundation
+
+// Network Response
+// Conform to Codable
+public protocol NetworkResponse: Codable { }
+
+
+// Network Request
+public struct NetworkRequest {
+    let method: HTTPMethod
+    let url: URL
+    var headers: [String: String]?
+    var body: Data?
+}
+
+public extension NetworkRequest {
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+}
+
+

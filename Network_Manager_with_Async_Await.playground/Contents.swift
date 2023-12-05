0fb4bcdbf8c5c852976fd10e647e48be1044bff6
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-05 07:32:17 +0530

    Defining network request sample

--- Network_Manager_with_Async_Await.playground/Contents.swift
+++ Network_Manager_with_Async_Await.playground/Contents.swift
@@ -67,3 +67,34 @@ public class NetworkManager {
             }
         }
 }
+
+
+//MARK: - Defining a NetworkRequest
+// Define the NetworkResponse
+struct Country: NetworkResponse {
+    let name: CountryName
+    
+    struct CountryName: Codable {
+        let common: String
+        let official: String
+    }
+}
+
+// Define how the NetworkRequest is created
+// Error 1 : No type for 'ResponseType' can satisfy both 'ResponseType == Array<Country>' and 'ResponseType : NetworkResponse'
+extension NetworkRequest where ResponseType == [Country] {
+    static func searchCountryByName(_ name: String) -> NetworkRequest<[Country]> {
+        let urlString = "https://restcountries.com/v3.1/name/\(name)?fullText=true"
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL: \(urlString)")
+        }
+        
+        return NetworkRequest<[Country]>(
+            method: .get,
+            url: url,
+            headers: nil,
+            body: nil
+        )
+    }
+}
+

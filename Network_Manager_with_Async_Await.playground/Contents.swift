db1ae225bb536bc7a76f9ec01d4c27a0a2913166
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-05 07:28:56 +0530

    NetworkManager class - performRequest

--- Network_Manager_with_Async_Await.playground/Contents.swift
+++ Network_Manager_with_Async_Await.playground/Contents.swift
@@ -36,4 +36,32 @@ public class NetworkManager {
         let configuration = URLSessionConfiguration.default
         session = URLSession(configuration: configuration)
     }
+    
+    func performRequest<ResponseType: NetworkResponse>(_ request: NetworkRequest<ResponseType>) async throws -> ResponseType {
+            // Create the URL object
+            var urlRequest = URLRequest(url: request.url)
+            urlRequest.httpMethod = request.method.rawValue
+            urlRequest.allHTTPHeaderFields = request.headers
+            urlRequest.httpBody = request.body
+            
+            // Fetch the data from the server
+            let (data, response) = try await session.data(for: urlRequest)
+            
+            // Check to make sure the httpResponse code is not outside of the 200...299 range
+            if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+                // If it is throw an error
+                throw NetworkError.httpError(statusCode: httpResponse.statusCode)
+            }
+            
+            // Try to decode your JSON response into your ResponseType object
+            do {
+                let decodedResponse = try JSONDecoder().decode(ResponseType.self, from: data)
+                // Return your object
+                return decodedResponse
+            } catch {
+                print(error)
+                // Throw if error during decoding
+                throw NetworkError.decodingError(error)
+            }
+        }
 }

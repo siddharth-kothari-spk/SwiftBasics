fab520a8f68e62e79eb279c10b73e4e3c088818f
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-12-09 12:39:31 +0530

    Custom Swift Logger - initial commit

---
+++ Custom Swift Logger/Custom Swift Logger/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Custom Swift Logger
+//
+//  Created by Siddharth Kothari on 09/12/23.
+//
+
+// courtsey: https://medium.com/@alessandromanilii/custom-swift-logger-in-xcode15-19da40a058a5
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

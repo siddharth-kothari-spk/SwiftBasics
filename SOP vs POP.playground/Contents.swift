b41cbd46ae103b6379866b282ce960e1dffd4ac3
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-17 13:25:14 +0530

    Replacing a protocol with an equivalent structure

--- SOP vs POP.playground/Contents.swift
+++ SOP vs POP.playground/Contents.swift
@@ -11,3 +11,66 @@ import UIKit
  Since the structure-oriented approach is based on structures that use static dispatch, the dispatch speed will be significantly different from protocols with dynamic dispatch in the protocol-oriented approach.
  */
 
+/*
+ 1. Replacing a protocol with an equivalent structure
+
+ In a protocol-oriented approach, the protocol itself allows abstraction from the implementation.
+
+ In the structure-oriented approach, generics and closures help to provide abstraction.
+ */
+
+
+// Abstraction
+protocol RegularProtocol {
+    var sqrValue: Int { get }
+}
+
+// Object
+class RegularClassProtocol {
+    
+    let value: Int
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+// Realization
+extension RegularClassProtocol: RegularProtocol {
+    var sqrValue: Int {
+        value * value
+    }
+}
+
+let onbjectProtocol = RegularClassProtocol(value: 25)
+print(onbjectProtocol.sqrValue)
+
+
+// Abstraction
+struct RegularStruct<AdoptedObject> {
+    var sqrValue: (AdoptedObject) -> Int
+    
+    init(sqrValue: @escaping (AdoptedObject) -> Int) {
+        self.sqrValue = sqrValue
+    }
+}
+
+// Object
+class RegularClassStruct {
+    
+    let value: Int
+    
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+// Realization
+extension RegularStruct where AdoptedObject == RegularClassStruct {
+    init() {
+        sqrValue = { object in object.value * object.value }
+    }
+}
+
+let onbjectStruct = RegularStruct<RegularClassStruct>()
+print(onbjectStruct.sqrValue(.init(value: 25)))

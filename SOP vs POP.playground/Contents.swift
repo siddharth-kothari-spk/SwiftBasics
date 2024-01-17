6cd57cf6e53048d274e068592b7f5494432f37b4
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-17 13:22:37 +0530

    SOP advantage over POP

--- SOP vs POP.playground/Contents.swift
+++ SOP vs POP.playground/Contents.swift
@@ -1,3 +1,13 @@
 // courtsey: https://itnext.io/structure-oriented-programming-vs-protocol-oriented-programming-in-swift-023970d80c75
 
 import UIKit
+
+/*
+ how to write code according to the SOLID principles without using protocols and without losing abstraction.
+
+ The Structure Oriented Programming is a paradigm based on the fact that any protocol can be replaced by a structure.
+
+ The advantages of this approach is performance.
+ Since the structure-oriented approach is based on structures that use static dispatch, the dispatch speed will be significantly different from protocols with dynamic dispatch in the protocol-oriented approach.
+ */
+

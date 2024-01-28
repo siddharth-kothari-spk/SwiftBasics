8a67c58c7f0626835d243b9b3b92a84c89cea663
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-01-28 16:56:21 +0530

    CSV to JSON initial commit

---
+++ CSV-To-JSON/CSV-To-JSON/CSV_To_JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSV_To_JSONApp.swift
+//  CSV-To-JSON
+//
+//  Created by Siddharth Kothari on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CSV_To_JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

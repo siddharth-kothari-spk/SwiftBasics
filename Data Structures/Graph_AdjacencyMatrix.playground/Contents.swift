95cd1a356147479099d937ff36d6662da12e4ef6
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-06 16:55:41 +0530

    Graph_AdjacencyMatrix: working

--- Data Structures/Graph_AdjacencyMatrix.playground/Contents.swift
+++ Data Structures/Graph_AdjacencyMatrix.playground/Contents.swift
@@ -22,7 +22,12 @@ public class AdjacencyMatrix<Element> {
     }
     return matrix[source][destination]
   }
-
+    
+    public func currentAdjacencyMatrix() {
+        for row in matrix {
+          print(row)
+        }
+    }
   // You can add methods for graph traversal (BFS, DFS) here (may require additional logic)
 }
 
@@ -33,13 +38,15 @@ var transportationNetwork = AdjacencyMatrix<Int>(vertices: 4) // 4 cities
 // Add edges (routes) with travel times between cities
 transportationNetwork.setEdge(100, from: 0, to: 1) // City 0 -> City 1 (travel time 100)
 transportationNetwork.setEdge(200, from: 0, to: 2) // City 0 -> City 2 (travel time 200)
+transportationNetwork.setEdge(250, from: 1, to: 2) // City 1 -> City 2 (travel time 200)
 transportationNetwork.setEdge(150, from: 1, to: 3) // City 1 -> City 3 (travel time 150)
 
 // Print the adjacency matrix (showing edge weights or nil for no connection)
-for row in transportationNetwork.matrix {
-  print(row)
-}
+transportationNetwork.currentAdjacencyMatrix()
 
 // Get travel time between City 0 and City 3 (indirect travel)
-let travelTime = transportationNetwork.getEdge(from: 0, to: 3) ?? -1 // Check for nil value
+var travelTime = transportationNetwork.getEdge(from: 0, to: 3) ?? -1 // Check for nil value
+print("Travel time from City 0 to City 3 (indirect): \(travelTime)")
+
+travelTime = transportationNetwork.getEdge(from: 1, to: 3) ?? -1 // Check for nil value
 print("Travel time from City 0 to City 3 (indirect): \(travelTime)")

7a3fb88d0666c57f8b523b53a69924c6e7470e97
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-04-17 06:13:17 +0530

    Phantom_Type: Example 2

--- Phantom_Type.playground/Contents.swift
+++ Phantom_Type.playground/Contents.swift
@@ -61,3 +61,58 @@ let combined1 = sample1 + sample3
 // not working code
 
 let combined2 = sample1 + sample2 // Error: Cannot convert value of type 'BloodSample<APositive>' to expected argument type 'BloodSample<OPositive>'
+
+// Example 2:
+
+//Sometimes the differences between types are less subtle. For example, consider a user struct like this one:
+
+struct User {
+    let id: Int
+    let age: Int
+}
+//Both id and age are integers, so there’s nothing stopping us from comparing them like this
+
+let user = User(id: 53, age: 53)
+print(user.id == user.age)
+// integers are identical, but if you’re comparing user IDs to ages then it’s almost certainly a mistake – and it’s a mistake phantom types can help us catch.
+
+
+enum UserID { }
+enum Age { }
+
+struct Tag<Type>: Equatable {
+    var value: Int
+}
+
+struct User_Phantom {
+    let id: Tag<UserID>
+    let age: Tag<Age>
+    
+    init(id: Tag<UserID>, age: Tag<Age>) {
+        self.id = id
+        self.age = age
+    }
+}
+
+let user2 = User_Phantom(id: Tag(value: 100), age: Tag(value: 100))
+
+print(user2.age == user2.id) // Error: Cannot convert value of type 'Tag<UserID>' to expected argument type 'Tag<Age>'
+
+
+// using ExpressibleByIntegerLiteral
+
+struct Tag2<Type>: Equatable, ExpressibleByIntegerLiteral {
+    init(integerLiteral value: IntegerLiteralType) {
+        self.value = value
+    }
+    var value: Int
+}
+
+struct User_Phantom2 {
+    let id: Tag2<UserID>
+    let age: Tag2<Age>
+}
+
+let user3 = User_Phantom2(id: Tag2(integerLiteral: 50), age: Tag2(integerLiteral: 70))
+
+print(user3.age == user3.id) // Error: Cannot convert value of type 'Tag2<UserID>' to expected argument type 'Tag2<Age>'

9fdada76d82e38f9b1323435d4e998a82f6ae6fe
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-03 07:16:52 +0530

    BackgroundTaskExample: added task identifier

--- BackgroundTaskExample/BackgroundTaskExample/BackgroundTask.swift
+++ BackgroundTaskExample/BackgroundTaskExample/BackgroundTask.swift
@@ -10,9 +10,10 @@ import BackgroundTasks
 
 class BackgroundTask {
     
+    public static let taskID = "com.siddharthkothari.BackgroundTaskExample.BG_Task"
     public static func registerBackgroundTask() {
         // register task
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: "", using: nil) { task in
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: taskID, using: nil) { task in
             // handle task
         }
         // schedule task

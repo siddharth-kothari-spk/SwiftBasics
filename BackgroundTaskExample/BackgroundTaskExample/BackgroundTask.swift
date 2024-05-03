1c01bac445c42a0a1a8f2b8f33193d21956575af
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-03 07:29:24 +0530

    BackgroundTaskExample: added methods for scheduling and handling task

--- BackgroundTaskExample/BackgroundTaskExample/BackgroundTask.swift
+++ BackgroundTaskExample/BackgroundTaskExample/BackgroundTask.swift
@@ -11,11 +11,35 @@ import BackgroundTasks
 class BackgroundTask {
     
     public static let taskID = "com.siddharthkothari.BackgroundTaskExample.BG_Task"
+    
     public static func registerBackgroundTask() {
         // register task
         BGTaskScheduler.shared.register(forTaskWithIdentifier: taskID, using: nil) { task in
             // handle task
+            guard let task = task as? BGAppRefreshTask else { return }
+            handleTask(task)
         }
         // schedule task
+    }
+    
+    private static func handleTask(_ task: BGAppRefreshTask) {
+        
+    }
+    
+    private static func scheduleTask() {
+        do {
+            let taskRequest = BGAppRefreshTaskRequest(identifier: taskID)
+            
+            /*!
+             @abstract The earliest date at which the task may run.
+             @discussion Setting this property does not guarantee that the task will begin at the specified date, but only that it will not begin sooner. If not specified, no start delay is used.
+             */
+            taskRequest.earliestBeginDate = Date().addingTimeInterval(86400 * 3)
+            
+            try BGTaskScheduler.shared.submit(taskRequest)
+        } catch {
+            
+        }
+        
     }
 }

bf153371ca8df370ab6a1005548c6b000b61e6e6
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2024-05-15 07:36:49 +0530

    Timers: Initialization

---
+++ Timers.playground/Contents.swift
@@ -0,0 +1,35 @@
+/*
+ https://vikramios.medium.com/swift-timer-24149096c0db
+ */
+
+// 1. Timer Initialization:
+
+import Foundation
+
+class MyTimerExample {
+    var timer: Timer?
+
+    func startTimer() {
+        // Create a timer that fires every 1 second
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
+        
+        // we create an instance of Timer using the scheduledTimer class method. This method automatically adds the timer to the current run loop, ensuring it fires at the specified interval.
+        /*
+         Let’s break down the parameters used in the scheduledTimer method:
+
+             timeInterval: The time between timer firings in seconds.
+             target: The object to which the selector belongs.
+             selector: The method to be called when the timer fires.
+             userInfo: Additional information to pass to the method specified by the selector.
+             repeats: A boolean indicating whether the timer should repeatedly fire at the specified interval.
+         */
+    }
+
+    @objc func timerFired() {
+        print("Timer fired!")
+    }
+}
+
+// Usage
+let myTimerExample = MyTimerExample()
+myTimerExample.startTimer()

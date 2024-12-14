14a16904695493bae239d10b642e15abae1c1dd7
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-14 21:47:33 +0530

    TodoItem_VIPER: interactor added

--- TodoItem_VIPER/TodoItem_VIPER/Interactor/Interactor.swift
+++ TodoItem_VIPER/TodoItem_VIPER/Interactor/Interactor.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+protocol Interactor {}
+
+protocol TodoInteractorOutput {
+    func didFetchTodoItems(_ items: [TodoItem])
+    func didAddTodoItem(_ item: TodoItem)
+    func didToggleTodoItem(_ item: TodoItem)
+}
+
+import Foundation
+class TodoInteractor: Interactor {
+    var output: TodoInteractorOutput!
+    var entity: TodoItem!
+
+    func fetchTodoItems() {
+        entity.fetchData { [weak self] items in
+            self?.output.didFetchTodoItems(items)
+        }
+    }
+
+    func addTodoItem(title: String) {
+        let item = TodoItem(id: UUID().hashValue, title: title)
+        entity.saveItem(item) { item in
+            self.output.didAddTodoItem(item)
+        }
+    }
+
+    func toggleTodoItem(item: TodoItem) {
+        item.itemChecked = !item.itemChecked
+        entity.saveItem(item) { item in
+            self.output.didToggleTodoItem(item)
+        }
+    }
+}

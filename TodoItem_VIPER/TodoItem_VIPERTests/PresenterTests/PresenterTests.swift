88c5ce6fd561c2694a5119e90a439e2ae8a60e3f
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-14 22:29:55 +0530

    TodoItem_VIPER: added presenter tests

--- TodoItem_VIPER/TodoItem_VIPERTests/PresenterTests/PresenterTests.swift
+++ TodoItem_VIPER/TodoItem_VIPERTests/PresenterTests/PresenterTests.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import XCTest
+@testable import TodoItem_VIPER
+
+class TodoPresenterTests: XCTestCase {
+    var presenter: TodoPresenter!
+    var view: TodoViewControllerMock!
+    
+    override func setUp() {
+        super.setUp()
+        presenter = TodoPresenter()
+        view = TodoViewControllerMock()
+        presenter.view = view
+    }
+    
+    func testUpdateView() {
+        let items = [TodoItem(id: 1, title: "Buy milk")]
+        presenter.updateView(with: items)
+        XCTAssertEqual(view.updateViewCalled, true)
+    }
+    
+    func testAddTodoItem() {
+        presenter.addTodoItem(title: "New Todo Item")
+        XCTAssertEqual(view.addButtonTappedCalled, true)
+    }
+    
+    func testToggleTodoItem() {
+        let item = TodoItem(id: 1, title: "Buy milk")
+        presenter.toggleTodoItem(at: 0)
+        XCTAssertEqual(view.toggleTodoItemCalled, true)
+    }
+}
--- TodoItem_VIPER/TodoItem_VIPERTests/PresenterTests/TodoViewControllerMock.swift
+++ TodoItem_VIPER/TodoItem_VIPERTests/PresenterTests/TodoViewControllerMock.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+@testable import TodoItem_VIPER
+
+class TodoViewControllerMock: TodoViewController {
+    var addButtonTappedCalled = false
+    var updateViewCalled = false
+    var toggleTodoItemCalled = false
+    
+    override func addButtonTapped(_ sender: UIButton) {
+        addButtonTappedCalled = true
+    }
+    
+    override func updateView(_ items: [TodoItem]) {
+        updateViewCalled = true
+    }
+    
+    override func toggleTodoItemAt(_ index: Int) {
+        toggleTodoItemCalled = true
+    }
+}

464f59f2826a5b76a7ed968ae414d1ebc52c4732
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-14 22:04:21 +0530

    TodoItem_VIPER: router added

---
+++ TodoItem_VIPER/TodoItem_VIPER/Router/Router.swift
@@ -0,0 +1,19 @@
+//
+//  Router.swift
+//  TodoItem_VIPER
+//
+//  Created by Siddharth Kothari on 14/12/24.
+//
+
+import Foundation
+
+protocol Router {}
+
+enum Route {}
+
+import UIKit
+    class TodoRouter: Router {
+        func navigate(to route: Route) {
+        // Handle navigation
+        }
+}

599deaa0d6dc95144144a46b7c4e979bd5dbb61a
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2024-12-14 21:54:48 +0530

    TodoItem_VIPER: view  added

---
+++ TodoItem_VIPER/TodoItem_VIPER/View/View.swift
@@ -0,0 +1,26 @@
+//
+//  View.swift
+//  TodoItem_VIPER
+//
+//  Created by Siddharth Kothari on 14/12/24.
+//
+
+import Foundation
+import UIKit
+
+class TodoViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var presenter: TodoPresenter!
+
+    var todoItems: [TodoItem] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = TodoPresenter()
+        presenter.interactor = TodoInteractor()
+    }
+
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        presenter.addTodoItem(title: "New Todo Item")
+    }
+}
